abe76b47397d1a02bc77e27d17381fe89cc87b06
Jerry Cheung <huahuazfh@qq.com>
2020-09-25 14:31:26 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,16 @@ set -e
 # 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# git
 git init
 git config user.name "Jerry Cheung"
 git config user.email "huahuazfh@qq.com"
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push
 git push -f git@github.com:bowlofnoodles/blog.git master:gh-pages
 
 cd -

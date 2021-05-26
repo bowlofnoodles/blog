2f2f0d34b52772193eafb3e70981060411684928
Jerry Cheung <huahuazfh@qq.com>
2021-05-26 10:46:34 +0800

    feat: 21-05-26-2 and code style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,6 +31,7 @@ module.exports = {
             '/algorithm/每日一题/21-05-24',
             '/algorithm/每日一题/21-05-25',
             '/algorithm/每日一题/21-05-26',
+            '/algorithm/每日一题/21-05-26-2',
           ]
         },
         {

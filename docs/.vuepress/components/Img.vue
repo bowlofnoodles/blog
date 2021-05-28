e56ee0714679c29b9b61c6632def4847a4bb3946
bowlofnoodles <huahuazfh@qq.com>
2021-05-28 21:58:52 +0800

    feat: 增加图片点击放大组件Img

---
+++ docs/.vuepress/components/Img.vue
@@ -0,0 +1,46 @@
+<template>
+  <img
+    v-bind="$attrs"
+    :src="src"
+    class="bowlofnoodles-img"
+    v-viewer
+  />
+</template>
+
+<script>
+import Viewer from 'v-viewer';
+import Vue from 'vue';
+
+Vue.use(Viewer);
+
+export default {
+  name: 'Img',
+  data() {
+    return {
+    };
+  },
+  props: {
+    src: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style src="viewerjs/dist/viewer.css" />
+
+<style lang="stylus">
+.bowlofnoodles-img {
+  cursor: zoom-in;
+}
+.viewer-prev, .viewer-next {
+  display: none;
+}
+.viewer-backdrop {
+  background-color: rgba(0, 0, 0, 0.75);
+}
+</style>
+
+
+
